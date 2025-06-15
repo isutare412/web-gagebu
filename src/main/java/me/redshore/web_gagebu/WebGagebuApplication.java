232cb17bbde0217bcb1cdcbf729e347e15e79720
Redshore <isutare412@gmail.com>
2025-06-15 22:50:47 +0900

    chore: bootstrap project

---
+++ src/main/java/me/redshore/web_gagebu/WebGagebuApplication.java
@@ -0,0 +1,13 @@
+package me.redshore.web_gagebu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGagebuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebGagebuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/redshore/web_gagebu/WebGagebuApplicationTests.java
@@ -0,0 +1,13 @@
+package me.redshore.web_gagebu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGagebuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
